f72a4b9f5710f61810e4dcdfdbcfbe4bb7ecfc18
ahmet92akinsel <ahmet.akinsel@gmail.com>
2021-02-22 17:19:24 +0200

    DELETE statements

---
+++ DELETE.sql
@@ -0,0 +1,12 @@
+SELECT * FROM orders;
+
+SELECT * FROM invoices;
+
+DELETE FROM invoices
+WHERE order_id= 30;
+
+SELECT * FROM order_details;
+DELETE FROM order_details
+
+WHERE order_id= 30;
+DELETE FROM orders WHERE id = 30;

9070bbc4b5d24266844d03e2f988fd8b285f1695
ahmet92akinsel <ahmet.akinsel@gmail.com>
2021-02-24 15:12:26 +0200

    Delete Statements

--- DELETE.sql
+++ DELETE.sql
@@ -1,12 +1,21 @@
 SELECT * FROM orders;
-
+SELECT * FROM order_details;
 SELECT * FROM invoices;
 
 DELETE FROM invoices
 WHERE order_id= 30;
 
-SELECT * FROM order_details;
-DELETE FROM order_details
 
+DELETE FROM order_details
 WHERE order_id= 30;
-DELETE FROM orders WHERE id = 30;
+
+DELETE FROM orders
+WHERE id = 30;
+
+
+DELETE FROM invoices
+WHERE
+shipping_address = 'New York';
+
+DROP TABLE employee;
+DROP DATABASE northwind;
